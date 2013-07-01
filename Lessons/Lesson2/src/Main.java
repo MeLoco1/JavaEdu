d5ba2d4333dd12e81a322b06119030711a7e6e44
MeLoco1 <dark_cdc@mail.ru>
2013-07-02 00:34:17 +0300

    Lesson2

--- Lessons/Lesson2/src/Main.java
+++ Lessons/Lesson2/src/Main.java
@@ -39,13 +39,44 @@ public class Main {
             }
         }
         // Сделать задачу 2 другим способом
+        {
+            System.out.println(" Задача 2, способ 2:");
+            String str = "Нужно Нужно просто просто просто разбить разбить это предложение на слова слова слова Нужно";
+            String[] str_arr = str.split(" ");
+            String[] tempstr_arr = new String[str_arr.length];
+            tempstr_arr[0] = str_arr[0];
+            int index = 1;
+
+            for (int i = 1; i < str_arr.length; i++) {
+                boolean isneededword = true;
+                for (int j = 0; j < tempstr_arr.length; j++) {
+                    if (str_arr[i].equals(tempstr_arr[j])) {
+                        isneededword = false;
+                        break;
+                    }
+                }
+                if (isneededword) {
+                    tempstr_arr[index] = str_arr[i];
+                    index++;
+                }
+            }
+            for (int i = 0; i < index; i++) {
+                int sum = 0;
+                for (int j = 0; j < str_arr.length; j++) {
+                    if (tempstr_arr[i].equals(str_arr[j])) {
+                        sum++;
+                    }
+                }
+                System.out.println("\"" + tempstr_arr[i] + "\" = " + sum);
+            }
+        }
         // Задача 3. Сортировка двумерного массива
         {
-            System.out.println(" Задача 3:");
+            //System.out.println(" Задача 3:");
             int[][] arr = new int[4][5];
             for (int i = 0; i < arr.length; i++) {
                 for (int j = 0; j < arr[i].length; j++) {
-                    arr[i][j] = (int)(Math.random() * 1000);
+                    arr[i][j] = (int) (Math.random() * 1000);
                 }
             }
 
@@ -53,3 +84,4 @@ public class Main {
         }
     }
 }
+
