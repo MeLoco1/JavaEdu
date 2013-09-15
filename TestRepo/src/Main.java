e95e259171ff56611057bbfe6c527bc5aa4aaf7c
MeLoco1 <anton.nsoft@gmail.com>
2013-09-15 17:13:14 +0300

    test

--- TestRepo/src/Main.java
+++
@@ -1,9 +0,0 @@
-import static net.mindview.util.Print.*;
-
-public class Main {
-
-    public static void main(String[] args) {
-        print("Test GIT");
-        //sout=System.out.println();
-    }
-}
